e38294252eef5a4e660541568c42d47811dd5fd1
ahmadsaif12 <ahmads87237@gmail.com>
2026-02-19 07:08:03 +0545

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import { defineConfig } from 'vite' 
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true, 
+      transformMixedEsModules: true,
     },
   },
+  server: {
+    historyApiFallback: true,
+  }
 })
